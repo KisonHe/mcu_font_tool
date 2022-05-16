921193fa45c50e6772088850e60cf009540c6ad4
KisonHe <kisonmaxine@gmail.com>
2022-05-16 23:07:29 +0800

    Delete test

--- test/jinjiatest.py
+++
@@ -1,8 +0,0 @@
-import jinja2
-from jinja2 import Template
-string_template_file = open("./test/strings_template.inja","r")
-string_template_string = string_template_file.read()
-string_template_file.close()
-
-template = Template(string_template_string)
-print(template.render(languages={"a","b"}))
